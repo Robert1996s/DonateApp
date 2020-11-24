50ef21ce67dfb42f333f76374893b4f6c212a2d5
bigrockquiz <bigrockquiz@gmail.com>
2020-11-24 13:42:29 +0100

    Second commit

--- app/src/main/java/com/example/donateapp/MainActivity.kt
+++ app/src/main/java/com/example/donateapp/MainActivity.kt
@@ -7,6 +7,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
+
+        val hej = 0
+
+        hej + 1
     }
 }
