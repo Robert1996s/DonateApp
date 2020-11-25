a4d4bad789d2cab169663702a2a403a923f2ca16
JacobEhvall <jacobehnvallgmail.com>
2020-11-25 20:34:47 +0100

    Added UI login page and some more declarations to them.

--- app/src/main/java/com/example/donateapp/MainActivity.kt
+++ app/src/main/java/com/example/donateapp/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.donateapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
@@ -11,12 +13,20 @@ class MainActivity : AppCompatActivity() {
     lateinit var db: FirebaseFirestore
     lateinit var auth: FirebaseAuth
 
+    lateinit var userTextView: EditText
+    lateinit var passwordTextView: EditText
+    lateinit var loginButton: Button
+    lateinit var memberButton: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         db = FirebaseFirestore.getInstance()
         auth = FirebaseAuth.getInstance()
+
+        var shoppingItems = mutableListOf<UserData>()
 
     }
 }
