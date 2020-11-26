09d6a976c0bf5d51fd4c545e84c148345b8b7d2d
bigrockquiz <bigrockquiz@gmail.com>
2020-11-26 14:56:30 +0100

    createuser function

--- app/src/main/java/com/example/donateapp/MainActivity.kt
+++ app/src/main/java/com/example/donateapp/MainActivity.kt
@@ -5,18 +5,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var db: FirebaseFirestore
-    lateinit var auth: FirebaseAuth
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         db = FirebaseFirestore.getInstance()
         auth = FirebaseAuth.getInstance()
 
@@ -26,6 +28,20 @@ class MainActivity : AppCompatActivity() {
 
         loginButton.setOnClickListener {
             toUserSignUp()
+        }
+
+        val currentUser: FirebaseUser? = auth.currentUser
+
+        if (currentUser != null) {
+            //User is logged in
+            println("!!!Logged IN")
+            //val intent = Intent(this,"Inloggade sidan"::class.java)
+            //startActivity(intent)
+
+        }
+        else {
+            //Not logged in
+            println("!!! NOT Logged IN")
         }
 
     } //ON CREATE
