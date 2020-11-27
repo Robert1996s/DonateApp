b0a1180e31a79882660ac64e49c2d46f0c3ed3b0
bigrockquiz <bigrockquiz@gmail.com>
2020-11-27 16:35:36 +0100

    Update db

--- app/src/main/java/com/example/donateapp/FirstPage.kt
+++ app/src/main/java/com/example/donateapp/FirstPage.kt
@@ -2,6 +2,7 @@ package com.example.donateapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
@@ -12,6 +13,7 @@ class FirstPage : AppCompatActivity() {
 
     lateinit var db: FirebaseFirestore
     lateinit var auth: FirebaseAuth
+    private var itemData : Items? = null
     
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,11 +25,31 @@ class FirstPage : AppCompatActivity() {
 
         val itemList = ArrayList<Items>()
 
-        val donateRecyclerview = findViewById<RecyclerView>(R.id.donate_list)
 
-        donateRecyclerview.layoutManager = LinearLayoutManager(this)
 
-        val adapter = RecyclerviewAdapter(itemList, this )
-        donateRecyclerview.adapter = adapter
+
+        //val donateRecyclerview = findViewById<RecyclerView>(R.id.donate_list)
+
+        //donateRecyclerview.layoutManager = LinearLayoutManager(this)
+
+        //val adapter = RecyclerviewAdapter(itemList, this )
+        //donateRecyclerview.adapter = adapter
+
+        getData()
+    }
+
+    private fun getData() {
+        val docRef = db.collection("items")
+            docRef.get()
+            .addOnSuccessListener { document ->
+                if (document != null){
+                    Log.d("exists","Data: ${document.data}")
+
+                }
+                println("!!!Done Getting data")
+
+            }
+
+
     }
 }
