697d3ee1b9d77ff7997f95f6667dc257390f609d
JacobEhvall <jacobehnvallgmail.com>
2020-12-16 15:46:54 +0100

    Possible to click into a new view from recyclerview

--- app/src/main/java/com/example/donateapp/ItemAdapter.kt
+++ app/src/main/java/com/example/donateapp/ItemAdapter.kt
@@ -2,6 +2,7 @@ package com.example.donateapp
 
 import android.content.ClipData
 import android.content.Context
+import android.content.Intent
 import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
@@ -14,11 +15,13 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_post_item.view.*
 import kotlinx.android.synthetic.main.row_card.view.*
+import java.lang.reflect.Array.get
+import java.nio.file.Paths.get
 
 class ItemAdapter (
+
     private val context: Context,
     private val items: List<Items>) : RecyclerView.Adapter<ItemAdapter.ViewHolder>() {
-
     //private var itemList: List<Items> = ArrayList()
 
     private val layoutInflater = LayoutInflater.from(context)
@@ -30,29 +33,43 @@ class ItemAdapter (
 
     override fun getItemCount() = items.size
 
-
-
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
         val item = items[position]
 
-            val itemTitle = item.title
-            val itemDesc = item.description
-            val itemImage = item.item_image_url
-            holder.titleText.text = itemTitle
-            holder.descriptionText.text = itemDesc
-            //holder.itemImage.setImageResource(item.item_image_url?)
-            //Glide.with(holder.itemView.).load(item).into(holder.itemView.item_image
+        val itemTitle = item.title
+        val itemDesc = item.description
+        val itemImage = item.item_image_url
+        holder.titleText.text = itemTitle
+        holder.descriptionText.text = itemDesc
+        //holder.itemPosition = position
 
-            //holder.itemImage.setImageResource(item.item_image_url))
+        holder.itemView.setOnClickListener {
+
+            val model = items[position]
+
+            val title : String? = model.title
+            val description : String? = model.description
+
+            val intent = Intent(context, DetalInformation::class.java)
+
+            intent.putExtra("titleText", title)
+            intent.putExtra("descriptionText", description)
+
+            context.startActivity(intent)
         }
-
-    inner class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
-        val titleText : TextView = itemView.findViewById(R.id.title_text)
-        val descriptionText : TextView = itemView.findViewById(R.id.description_text)
-        val itemImage : ImageView = itemView.findViewById(R.id.item_image)
-
     }
 
+    //holder.itemImage.setImageResource(item.item_image_url?)
+    //Glide.with(holder.itemView.).load(item).into(holder.itemView.item_image
+    //holder.itemImage.setImageResource(item.item_image_url))
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val titleText: TextView = itemView.findViewById(R.id.title_text)
+        val descriptionText: TextView = itemView.findViewById(R.id.description_text)
+        val itemImage: ImageView = itemView.findViewById(R.id.item_image)
+        //var itemPosition = 0
+
+    }
 }
 
